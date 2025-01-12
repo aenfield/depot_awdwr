5b2904650e31de18f58d477efae1ab694c96786d
Andrew Enfield <aenfield@gmail.com>
2025-01-11 17:14:48 -0800

    Added internationalization plumbing via routes, and updated nav layout, store, and cart text.

--- config/routes.rb
+++ config/routes.rb
@@ -5,19 +5,20 @@ Rails.application.routes.draw do
     post "login" => :create
     delete "logout" => :destroy
   end
-
-  resources :users
-  resources :orders
-  resources :line_items
-  resources :carts
-  root "store#index", as: "store_index"
-  resources :products
-
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-
   # Render dynamic PWA files from app/views/pwa/*
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
+
+  resources :users
+  resources :products
+
+  scope "(:locale)" do
+    resources :orders
+    resources :line_items
+    resources :carts
+    root "store#index", as: "store_index", via: :all
+  end
 end
