08a951a4393dfb18eb4c47df6d19cf4006aab20f
Andrew Enfield <aenfield@gmail.com>
2025-01-12 11:11:04 -0800

    Tried updating routes from book code to see if it resolves case where locale isn't used by all controllers (per Rails docs) - didn't work.

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,12 @@
 Rails.application.routes.draw do
   get "admin" => "admin#index"
   controller :sessions do
-    get "login" => :new
+    get  "login" => :new
     post "login" => :create
     delete "logout" => :destroy
   end
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-  # Render dynamic PWA files from app/views/pwa/*
-  get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
-  get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
+  get "sessions/create"
+  get "sessions/destroy"
 
   resources :users
   resources :products
